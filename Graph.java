135d6bbf4a168c6ad5b00e400f6e3379e19998a6
Matthew Thomas <mthoma23@uvm.edu>
2022-10-18 22:17:49 -0400

    DONE

--- Graph.java
+++ Graph.java
@@ -1,119 +1,10 @@
-<<<<<<< HEAD
-// CS224 Fall 2022
-
-import java.util.ArrayList;
-import java.util.Queue;
-
-public class Graph {
-  ArrayList<Node> nodes;
-
-  public Graph() {
-    this.nodes = new ArrayList<Node>();
-  }
-
-  public void addNode(Node newNode) {
-    for (Node n: this.nodes) {
-      if (n == newNode) {
-        System.out.println("ERROR: graph already has a node " + n.name);
-        assert false;
-      }
-    }
-    nodes.add(newNode);
-  }
-
-  public void addEdge(Node n1, Node n2, int weight) {
-    // make sure edge does not already exist
-    int idx1 = this.nodes.indexOf(n1);
-    if (idx1 >= 0) {
-      for (Link link: this.nodes.get(idx1).adjlist) {
-        if (link.n2 == n2) {
-          System.out.println("ERROR: there is already an edge from " + n1.name + " to " + n2.name);
-          return;
-        }
-      }
-      this.nodes.get(idx1).addEdge(n2, weight);
-    } else {
-      System.out.println("ERROR: node " + n1.name + " not found in graph");
-    }
-
-    int idx2 = this.nodes.indexOf(n2);
-    if (idx2 >= 0) {
-      this.nodes.get(idx2).addEdge(n1, weight);
-    } else {
-      System.out.println("ERROR: node " + n2.name + " not found in graph");
-    }
-  } // addEdge()
-
-  public void print() {
-    for (Node n1: this.nodes) {
-      System.out.print(n1 + ":");
-      for (Link link: n1.adjlist) {
-        System.out.print(" " + link.n2.name + " (d=" + link.weight + ")");
-      }
-      System.out.print("|");
-    }
-  }
-
-  public int[] shortestPath(Node s) {
-    // implement this
-    //have grpah of nodes 
-    //given starting node
-    ArrayList<Node> explored = new ArrayList<Node>();
-    int distStart = 0;
-    explored.add(s);
-    while (explored.size() != nodes.size()){
-      for (Node n : explored){                
-        for (Link l : n.adjlist) {
-          //PRINT list of 'found nodes' - > 'node' : 'PATH TO start node'
-          int path = dist
-          System.out.print(n.name "->" l.n2.name ": " + distances[i]);
-          //add the weight to the distance to staring node
-          
-          
-          
-        }
-      }
-    }
-    
-// Set Discovered[s] = true and Discovered[v] = false for all other v
-// Initialize L[0] to consist of the single element s
-// Set the layer counter i = 0
-// Set the current BFS tree T = ∅
-// While L[i] is not emptyrrrrr
-// Initialize an empty list L[i + 1]
-// For each node u ∈ L[i]
-// Consider each edge (u, v) incident to u
-// If Discovered[v] = false then
-// Set Discovered[v] = true
-// Add edge (u, v) to the tree T
-// Add v to the list L[i + 1]
-// Endif
-// Endfor
-// Increment the layer counter i by one
-// Endwhile
-
-
-
-    //PRINT list of 'found nodes' - > 'node' : 'PATH TO start node'
-    //PRINT found a node "" : distance is ""
-    //Prints node from the list with shortest path to start
-
-    //return array of ints 
-    //each index is the distance to that node 
-    //[0,1,2,3,2,4]
-
-  } // shortestPath()
-
-
-
-}
-=======
 // CS224 Fall 2022
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+
 public class Graph {
   ArrayList<Node> nodes;
 
@@ -165,22 +56,46 @@ public class Graph {
   }
 
   public int[] shortestPath(Node s) {
-    int[]Distances = new int[nodes.size()];
+    int[]distances = new int[nodes.size()+1];
+    
     boolean[]explored = new boolean[nodes.size()];
-    Queue<Node> Tree = new LinkedList();
-    Tree.add(s);
-    int distStart = 0;
-    Distances[s.name-1] = distStart;
-    while(!Tree.isEmpty()){
-      Node current = (Tree.remove());
-      Distances[current.name-1] = 
-      explored[0] = true;
-      for (Link l : current.adjlist){
-        System.out.print(current.name "->" l.n2.name "=" l.weight + Distances[current.name-1]);
-
+    int startDist = 0;
+    distances[s.name] = startDist;
+    explored[s.name-1] = true;
+    
+    ArrayList<Node>beenTo = new ArrayList<Node>(); 
+    beenTo.add(s);
+    ArrayList<Node>visited = new ArrayList<Node>();
+    visited.add(s);
+    
+    while(beenTo.size() != nodes.size()){ 
+      
+      Node toAdd= new Node(0);
+      int temp = 10000;
+      
+      for (Node n : visited){
+        int distStart = distances[n.name];        
+                
+        for (Link l : n.adjlist){
+          if(!visited.contains(l.n2)){
+          Node other = l.n2; 
+          int dist = l.weight + distStart;
+          System.out.println(n.name + "->" + other.name + "=" + dist);
+            if (dist < temp){
+              temp = dist;
+              toAdd = other;
+            }          
+          }
+        }
+        
       }
+      System.out.println("found a node " + toAdd.name + ": distance is " + temp);     
+      distances[toAdd.name] = temp;
+      explored[toAdd.name-1] = true;
+      beenTo.add(toAdd);
+      visited.add(toAdd);
     }
+    return distances;
     // implement this
   } // shortestPath()
-}
->>>>>>> 5dbed774c9f11b44c35a8b771539a651e3d1f5ff
+}//when you add a node to visited add its distance
